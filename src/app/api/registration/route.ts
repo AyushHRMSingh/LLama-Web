5ea1091767315b1e666438f1e48401a68f3bb9b8
Heckrexter <102521099+Heckrexter@users.noreply.github.com>
2024-08-21 18:49:55 +0530

    Breaking down project into multiple

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -4,13 +4,14 @@ import { headers } from 'next/headers';
 
 
 export async function POST(request: NextRequest) {
-  const userdata = await request.json();
-  const { email, password, accessToken } = userdata;
+  // const userdata = await request.json();
+  // const { email, password, accessToken } = userdata;
   
   // console.log(email, password, accessToken, request.url);
   // const response = await registerResource(email, password, accessToken, request.headers);
   const response = headers().get("x-forwarded-for");
   console.log(response);
+  let res = response?.split(":")[3];
 
   return new Response("response", { status: 200 });
 }
