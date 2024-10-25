a55112dfe9a46f7feef448e3462b2f9a2ad22e7b
Heckrexter <102521099+Heckrexter@users.noreply.github.com>
2024-10-25 12:32:17 +0530

    fix user-actions

--- src/context/user.context.tsx
+++ src/context/user.context.tsx
@@ -2,11 +2,6 @@
 "use client"
 import { useState, createContext, useEffect, useContext } from 'react';
 import { auth, registerUserWithEmailAndPassword } from '@/lib/firebase';
-import  {
-  addUser,
-  removeUser,
-  getUser
-} from "@/functions/user-actions"
 import Cookies from 'js-cookie';
 
 const AuthContext = createContext();
@@ -20,8 +15,6 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
   const [currentUser, setCurrentUser] = useState<any>(() => {
-    // const storedUser = localStorage.getItem('currentUser');
-    // const storedUser = getUser()
     const storedUser = Cookies.get('currentuser')
     return storedUser ? JSON.parse(JSON.stringify(storedUser)) : null;
   });
@@ -29,7 +22,6 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        // console.log('User logged in:', JSON.stringify(user));
         Cookies.set('currentuser', JSON.stringify(user), { path: '/' });
       } else {
         Cookies.remove('currentuser')
